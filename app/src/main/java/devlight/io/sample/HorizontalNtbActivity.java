c9c0d3914da7d4cb54c83ec5f094df6ff7c1457b
lyj789 <lyj981014@163.com>
2019-05-24 08:58:07 +0800

    merge

--- app/src/main/java/devlight/io/sample/HorizontalNtbActivity.java
+++ app/src/main/java/devlight/io/sample/HorizontalNtbActivity.java
@@ -1,6 +1,7 @@
 package devlight.io.sample;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -50,6 +51,9 @@ public class HorizontalNtbActivity extends Activity {
         view_container.add(page3_lemon);
 
 
+        startActivity(new Intent(HorizontalNtbActivity.this, lemontime.class));
+
+
         viewPager.setAdapter(new PagerAdapter() {
             @Override
             public int getCount() {
@@ -68,7 +72,6 @@ public class HorizontalNtbActivity extends Activity {
 
             @Override
             public Object instantiateItem(final ViewGroup container, final int position) {
-
                 container.addView(view_container.get(position));
                 return view_container.get(position);
             }
@@ -142,105 +145,4 @@ public class HorizontalNtbActivity extends Activity {
             }
         }, 500);
     }
-
-
-//    private void initUI()  {
-//        final ViewPager viewPager = (ViewPager) findViewById(R.id.vp_horizontal_ntb);
-//        viewPager.setAdapter(new PagerAdapter() {
-//            @Override
-//            public int getCount() {
-//                return 5;
-//            }
-//
-//            @Override
-//            public boolean isViewFromObject(final View view, final Object object) {
-//                return view.equals(object);
-//            }
-//
-//            @Override
-//            public void destroyItem(final View container, final int position, final Object object) {
-//                ((ViewPager) container).removeView((View) object);
-//            }
-//
-//            @Override
-//            public Object instantiateItem(final ViewGroup container, final int position) {
-//                final View view = LayoutInflater.from(
-//                        getBaseContext()).inflate(R.layout.item_vp, null, false);
-//
-//                final TextView txtPage = (TextView) view.findViewById(R.id.txt_vp_item_page);
-//                txtPage.setText(String.format("Page #%d", position));
-//
-//                container.addView(view);
-//                return view;
-//            }
-//        });
-//
-//        final String[] colors = getResources().getStringArray(R.array.default_preview);
-//
-//        final NavigationTabBar navigationTabBar = (NavigationTabBar) findViewById(R.id.ntb_horizontal);
-//        final ArrayList<NavigationTabBar.Model> models = new ArrayList<>();
-//        models.add(
-//                new NavigationTabBar.Model.Builder(
-//                        getResources().getDrawable(R.drawable.ic_list),
-//                        Color.parseColor(colors[3]))
-////                        .selectedIcon(getResources().getDrawable(R.drawable.ic_eighth))
-//                        .title("List")
-//                        .badgeTitle("state")
-//                        .build()
-//        );
-//
-//        models.add(
-//                new NavigationTabBar.Model.Builder(
-//                        getResources().getDrawable(R.drawable.ic_calendar),
-//                        Color.parseColor(colors[1]))
-////                        .selectedIcon(getResources().getDrawable(R.drawable.ic_eighth))
-//                        .title("Calendar")
-//                       // .badgeTitle("with")
-//                        .build()
-//        );
-//        models.add(
-//                new NavigationTabBar.Model.Builder(
-//                        getResources().getDrawable(R.drawable.ic_lemon),
-//                        Color.parseColor(colors[2]))
-//                        .title("Lemon Tree")
-//                      //  .badgeTitle("with")
-//                        .build()
-//        );
-//
-//
-//
-//        navigationTabBar.setModels(models);
-//        navigationTabBar.setViewPager(viewPager, 2);
-//        navigationTabBar.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-//            @Override
-//            public void onPageScrolled(final int position, final float positionOffset, final int positionOffsetPixels) {
-//
-//            }
-//
-//            @Override
-//            public void onPageSelected(final int position) {
-//                navigationTabBar.getModels().get(position).hideBadge();
-//            }
-//
-//            @Override
-//            public void onPageScrollStateChanged(final int state) {
-//
-//            }
-//        });
-//
-//        navigationTabBar.postDelayed(new Runnable() {
-//            @Override
-//            public void run() {
-//                for (int i = 0; i < navigationTabBar.getModels().size(); i++) {
-//                    final NavigationTabBar.Model model = navigationTabBar.getModels().get(i);
-//                    navigationTabBar.postDelayed(new Runnable() {
-//                        @Override
-//                        public void run() {
-//                            //model.showBadge();
-//                        }
-//                    }, i * 100);
-//                }
-//            }
-//        }, 500);
-//    }
 }
---
+++ app/src/main/java/devlight/io/sample/lemontime.java
@@ -0,0 +1,36 @@
+package devlight.io.sample;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import devlight.io.library.ntb.NavigationTabBar;
+
+
+public class lemontime extends Activity {
+    private TextView textview;
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.page3_lemon);
+
+        button = (Button) findViewById(R.id.begin);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textview = (TextView) findViewById(R.id.time);
+                String myText = textview.getText().toString();
+                textview.setText("LYJ的" + myText);
+            }
+        });
+    }
+}
+
