adf7961ea4f7fad1db05a088412039dda793265f
CielChan <2524497642@qq.com>
2019-05-27 15:06:59 +0800

    PageTodolist connect to Database

--- app/src/main/java/devlight/io/sample/PageTodolist.java
+++ app/src/main/java/devlight/io/sample/PageTodolist.java
@@ -3,7 +3,10 @@ package devlight.io.sample;
 import android.animation.Animator;
 import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.DatabaseUtils;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
@@ -21,6 +24,8 @@ import android.widget.ListView;
 import java.util.ArrayList;
 import java.util.List;
 
+import devlight.io.sample.components.MySQLiteOpenHelper;
+
 
 public class PageTodolist extends Fragment implements ListAdapter.InnerItemOnclickListener {
 
@@ -28,46 +33,84 @@ public class PageTodolist extends Fragment implements ListAdapter.InnerItemOncli
     private List<ListAdapter.itemHolder> list = new ArrayList<ListAdapter.itemHolder>();
     private ListView todoList;
     private ListAdapter listAdapter;
-    private FloatingActionButton addbutton;
+    private MySQLiteOpenHelper dbHelper;
+    private int insertPosition = 1;
+
+
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.page1_todolist, container, false);
+        dbHelper = MySQLiteOpenHelper.getInstance(getContext());
 
-        ListAdapter.itemHolder test1 = new ListAdapter.itemHolder();
-        test1.text = "主人~ 努力啊，还有这么多没完成呢~";
-        test1.type = 0;
-        list.add(test1);
+        ListAdapter.itemHolder title_undo = new ListAdapter.itemHolder();
+        title_undo.text = "主人~ 努力啊，还有这么多没完成呢~";
+        title_undo.type = 0;
+        list.add(title_undo);
 
-
-        ListAdapter.itemHolder test2;
-        for (int i = 0; i < 10; i++) {
-            test2 = new ListAdapter.itemHolder();
-            test2.text = "todolist" + i;
-            test2.time = i + ":00 pm";
-            test2.btn = R.drawable.ic_button_off;
-            test2.type = 1;
-            list.add(test2);
+        ListAdapter.itemHolder item_undo;
+        String sql = "SELECT * FROM tb_todo WHERE is_done==0";
+        Cursor cursor = dbHelper.getWritableDatabase().rawQuery(sql, null);
+        ContentValues cv_undo = new ContentValues();
+        while (cursor.moveToNext()) {
+            item_undo = new ListAdapter.itemHolder();
+            DatabaseUtils.cursorStringToContentValues(cursor, "title", cv_undo);
+            DatabaseUtils.cursorIntToContentValues(cursor, "alert_time", cv_undo);
+            DatabaseUtils.cursorIntToContentValues(cursor,"id",cv_undo);
+            item_undo.text = cv_undo.getAsString("title");
+            item_undo.time = cv_undo.getAsString("alert_time");
+            item_undo.type = 1;
+            item_undo.id = cv_undo.getAsInteger("id");
+            list.add(item_undo);
+            insertPosition++;
         }
 
-        ListAdapter.itemHolder test3 = new ListAdapter.itemHolder();
-        test3.text = "哇哦~超级棒!";
-        test3.type = 0;
-        list.add(test3);
 
-        ListAdapter.itemHolder test4;
-        for (int i = 0; i < 10; i++) {
-            test4 = new ListAdapter.itemHolder();
-            test4.text = "alreadydolist" + i;
-            test4.time = i + ":pm";
-            test4.btn = R.drawable.ic_button_on;
-            test4.type = 2;
-            list.add(test4);
+
+//        for (int i = 0; i < 10; i++) {
+//            test2 = new ListAdapter.itemHolder();
+//            test2.text = "todolist" + i;
+//            test2.time = i + ":00 pm";
+//            test2.btn = R.drawable.ic_button_off;
+//            test2.type = 1;
+//            list.add(test2);
+//        }
+
+        ListAdapter.itemHolder tile_done = new ListAdapter.itemHolder();
+        tile_done.text = "哇哦~超级棒!";
+        tile_done.type = 0;
+        list.add(tile_done);
+
+
+        ListAdapter.itemHolder item_done;
+        sql = "SELECT * FROM tb_todo WHERE is_done==1";
+        cursor = dbHelper.getWritableDatabase().rawQuery(sql, null);
+        ContentValues cv_done = new ContentValues();
+        while (cursor.moveToNext()) {
+            item_done = new ListAdapter.itemHolder();
+            DatabaseUtils.cursorStringToContentValues(cursor, "title", cv_done);
+            DatabaseUtils.cursorIntToContentValues(cursor, "alert_time", cv_done);
+            DatabaseUtils.cursorIntToContentValues(cursor,"id",cv_done);
+            item_done.text = cv_done.getAsString("title");
+            item_done.time = cv_done.getAsString("alert_time");
+            item_done.type = 2;
+            item_done.id = cv_done.getAsInteger("id");
+            list.add(item_done);
         }
+//
+//        for (int i = 0; i < 10; i++) {
+//            test4 = new ListAdapter.itemHolder();
+//            test4.text = "alreadydolist" + i;
+//            test4.time = i + ":00 pm";
+//            test4.btn = R.drawable.ic_button_on;
+//            test4.type = 2;
+//            list.add(test4);
+//        }
 
 
         todoList = view.findViewById(R.id.todolist);
 
+        FloatingActionButton addbutton;
         addbutton = view.findViewById(R.id.add_btn);
         listAdapter = new ListAdapter(getActivity(), R.layout.item_todolist, list, todoList);
         listAdapter.setOnInnerItemOnClickListener(this);
@@ -166,7 +209,27 @@ public class PageTodolist extends Fragment implements ListAdapter.InnerItemOncli
 
     private void remove(int position) {
         if (position < list.size()) {
-            list.remove(position);
+            if(list.get(position).type == 1){
+                ContentValues values = new ContentValues();
+                values.put("is_done",1);
+                dbHelper.getWritableDatabase().update("tb_todo",values,"id=?",new String[]{list.get(position).id+""});
+                insertPosition--;
+                ListAdapter.itemHolder change_item =  list.get(position);
+                list.remove(position);
+                change_item.type = 2;
+                list.add(change_item);
+
+            }else{
+                ContentValues values = new ContentValues();
+                values.put("is_done",0);
+                dbHelper.getWritableDatabase().update("tb_todo",values,"id=?",new String[]{list.get(position).id+""});
+                ListAdapter.itemHolder change_item =  list.get(position);
+                list.remove(position);
+                change_item.type = 1;
+                list.add(insertPosition,change_item);
+                insertPosition++;
+            }
+
         }
         listAdapter.notifyDataSetChanged();
     }
